5df714c0081cd56c3e43834a5c3f61c6f87048f3
hara0907 <y_hara0907@outlook.jp>
2023-06-19 07:22:02 +0900

    課題三章再提出

--- Main.java
+++ Main.java
@@ -10,18 +10,18 @@ public class Main{
         * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
         */
 
-        int data[]={3,1,2,7,5}
+        int data[]={3,1,2,7,5};
 
         /*
         * 問2
         * 以下のfor文を完成させなさい
         */
-        for(int i = 0; i < data length; i++){
+        for(int i = 0; i < data.length; i++){
             System.out.print(data[i] + " ");
         }
         System.out.println();
-        for (int i = 0; i < data length -1; i++) {
-            for (int j = data length -1; j > i; j--) {
+        for (int i = 0; i < data.length -1; i++) {
+            for (int j = data.length -1; j> i; j--) {
                 /*
                 * 問3
                 * 以下、配列の添字を入れてソートを完成させなさい
